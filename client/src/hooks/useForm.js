7755f9c6a2fd35723e563b9d9bacf95a81274b77
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-10-10 14:18:26 -0400

    Done with checkoutForm component

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,8 @@
 // write your custom hook here to control your checkout form
+import { useLocalStorage } from './useLocalStorage';
+
+export const useForm = (initialvalue) => {
+    const [values, setValues] = useLocalStorage("contactFormHook", initialvalue);
+   
+    return [values, setValues];
+}
